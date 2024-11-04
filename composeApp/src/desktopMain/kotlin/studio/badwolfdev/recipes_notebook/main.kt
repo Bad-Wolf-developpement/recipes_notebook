b261fa229ecd5bc17b881a0c9414bcb529da80bd
arist0v <arist0v@badwolfdev.studio>
2024-11-04 08:32:20 -0500

    restart the project code to use less KMP option

--- composeApp/src/desktopMain/kotlin/studio/badwolfdev/recipes_notebook/main.kt
+++ composeApp/src/desktopMain/kotlin/studio/badwolfdev/recipes_notebook/main.kt
@@ -6,7 +6,7 @@ import androidx.compose.ui.window.application
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "recipes_notebook",
+        title = "Recipes Notebook",
     ) {
         App()
     }
--- composeApp/src/iosMain/kotlin/studio/badwolfdev/recipes_notebook/MainViewController.kt
+++
@@ -1,5 +0,0 @@
-package studio.badwolfdev.recipes_notebook
-
-import androidx.compose.ui.window.ComposeUIViewController
-
-fun MainViewController() = ComposeUIViewController { App() }
