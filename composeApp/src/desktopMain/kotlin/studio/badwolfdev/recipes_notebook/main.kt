2cb9239c9d0232c11bf8948275270e94d75c18cd
arist0v <arist0v@badwolfdev.studio>
2024-11-05 08:41:25 -0500

    set default value to desktop windows app

--- composeApp/src/desktopMain/kotlin/studio/badwolfdev/recipes_notebook/main.kt
+++ composeApp/src/desktopMain/kotlin/studio/badwolfdev/recipes_notebook/main.kt
@@ -1,12 +1,18 @@
 package studio.badwolfdev.recipes_notebook
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
+    val state = rememberWindowState() // default windows configuration
     Window(
+        state = state,
         onCloseRequest = ::exitApplication,
-        title = "recipes_notebook",
+        title = "recipes_notebook", // TODO change this to res strings
+        //icon = Painter?, TODO Add icon to the windows
+
     ) {
         App()
     }
