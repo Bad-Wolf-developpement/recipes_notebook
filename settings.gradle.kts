2d47d921768809bb3f08d10403152efedeca275a
arist0v <arist0v@users.noreply.github.com>
2024-11-05 07:53:13 -0500

    Regenerate code (#4)
    Co-authored-by: arist0v <arist0v@badwolfdev.studio>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "recipes_notebook"
+rootProject.name = "RecipesNotebook"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
@@ -29,5 +29,3 @@ dependencyResolutionManagement {
 }
 
 include(":composeApp")
-include(":server")
-include(":shared")
--- shared/src/commonMain/kotlin/studio/badwolfdev/recipes_notebook/Constants.kt
+++
@@ -1,3 +0,0 @@
-package studio.badwolfdev.recipes_notebook
-
-const val SERVER_PORT = 8080
