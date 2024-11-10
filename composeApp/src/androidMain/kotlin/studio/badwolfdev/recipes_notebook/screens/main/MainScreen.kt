8b15345f42d846a512d6d556df9f1e453387dd79
arist0v <arist0v@badwolfdev.studio>
2024-11-10 16:33:20 -0500

    make some more fix

--- composeApp/src/androidMain/kotlin/studio/badwolfdev/recipes_notebook/screens/main/MainScreen.kt
+++ composeApp/src/androidMain/kotlin/studio/badwolfdev/recipes_notebook/screens/main/MainScreen.kt
@@ -3,6 +3,9 @@ package studio.badwolfdev.recipes_notebook.screens.main
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import org.jetbrains.compose.resources.stringResource
+import recipesnotebook.composeapp.generated.resources.Res
+import recipesnotebook.composeapp.generated.resources.app_name
 import studio.badwolfdev.recipes_notebook.presentation.TopAppBar
 
 @Composable
@@ -11,7 +14,8 @@ fun MainScreen(
 ){
     Column {
         TopAppBar(
-            title = "TEMP TITLE"
+            title =
+            stringResource(Res.string.app_name)
         )
 
         Text("Main Screen")
