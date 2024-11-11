384d5c9881293d941f194d6f58de01cf7d2aee60
arist0v <arist0v@users.noreply.github.com>
2024-11-11 09:40:46 -0500

    completed a Navigation for desktop as suggested by chatGpt (#13)
    Co-authored-by: arist0v <arist0v@badwolfdev.studio>

--- composeApp/src/androidMain/kotlin/studio/badwolfdev/recipes_notebook/screens/AppScafold.android.kt
+++ composeApp/src/androidMain/kotlin/studio/badwolfdev/recipes_notebook/screens/AppScafold.android.kt
@@ -5,17 +5,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import studio.badwolfdev.recipes_notebook.screens.about.AboutScreen
 import studio.badwolfdev.recipes_notebook.screens.main.MainScreen
 
 @Composable
-fun AppScaffold(
-
-){
+actual fun AppScaffold() {
     Scaffold(
 
     ) { paddingValues ->
---
+++ composeApp/src/commonMain/kotlin/studio/badwolfdev/recipes_notebook/screens/AppScaffold.kt
@@ -0,0 +1,6 @@
+package studio.badwolfdev.recipes_notebook.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+expect fun AppScaffold()
