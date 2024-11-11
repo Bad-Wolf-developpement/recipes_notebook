8c3d7190b90661521453fb8d0e35dcddc6de7799
arist0v <arist0v@users.noreply.github.com>
2024-11-11 08:40:09 -0500

    9 refactoring for new knowledge (#12)
    Co-authored-by: arist0v <arist0v@badwolfdev.studio>

---
+++ composeApp/src/androidMain/kotlin/studio/badwolfdev/recipes_notebook/screens/AppScaffold.kt
@@ -0,0 +1,48 @@
+package studio.badwolfdev.recipes_notebook.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import studio.badwolfdev.recipes_notebook.screens.about.AboutScreen
+import studio.badwolfdev.recipes_notebook.screens.main.MainScreen
+
+@Composable
+fun AppScaffold(
+
+){
+    Scaffold(
+
+    ) { paddingValues ->
+        val navController = rememberNavController()
+        AppNavHost(
+            navController = navController,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        )
+    }
+}
+
+@Composable
+fun AppNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(
+        navController = navController,
+        startDestination = Screens.MAIN.route
+    ){
+        composable(Screens.MAIN.route){
+            MainScreen()
+        }
+        composable(Screens.ABOUT.route){
+            AboutScreen()
+        }
+    }
+}
