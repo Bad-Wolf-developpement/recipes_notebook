a608542c3ab54870254f15e01ed96cd441f98d90
arist0v <arist0v@users.noreply.github.com>
2024-11-11 09:55:41 -0500

    add nav tree for wasm (#15)
    Co-authored-by: arist0v <arist0v@badwolfdev.studio>

--- composeApp/src/wasmJsMain/kotlin/studio/badwolfdev/recipes_notebook/screens/AppScafold.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/studio/badwolfdev/recipes_notebook/screens/AppScafold.wasmJs.kt
@@ -1,9 +1,22 @@
 package studio.badwolfdev.recipes_notebook.screens
 
 import androidx.compose.runtime.Composable
-import studio.badwolfdev.recipes_notebook.Content
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import studio.badwolfdev.recipes_notebook.screens.about.AboutScreen
+import studio.badwolfdev.recipes_notebook.screens.main.MainScreen
 
 @Composable
 actual fun AppScaffold() {
-    Content()
+    val currentScreen by remember { mutableStateOf(Screens.MAIN.route) }
+
+    when (currentScreen){
+        Screens.MAIN.route -> {
+            MainScreen()
+        }
+        Screens.ABOUT.route -> {
+            AboutScreen()
+        }
+    }
 }
